8e6ff15bd0a44fe2bca1e3be11d02089519b9b8f
furysun <fury.sun.main@gmail.com>
2017-08-08 15:28:57 +0300

    I changed the values in places in two ways.

--- src/com/company/start/inheritance/Child.java
+++ src/com/company/start/inheritance/Child.java
@@ -1,10 +1,5 @@
 package com.company.start.inheritance;
 
-import com.company.start.interfaceDemo.Parent;
-
-/**
- * Created by yana on 8/7/17.
- */
 class Child extends Parent {
 
 }
--- src/com/company/start/swapVariables/SewapVaribles.java
+++ src/com/company/start/swapVariables/SewapVaribles.java
@@ -2,6 +2,23 @@ package com.company.start.swapVariables;
 
 public class SewapVaribles {
     public static void main(String[] args) {
+        int a = 1;
+        int b = 3;
 
+        int c = a;
+        a = b;
+        b = c;
+
+        System.out.println(a + " " + b);
+
+        System.out.println("Without a third variable.");
+        int d = 4;
+        int e = 1;
+
+        d = d + e;
+        e = d - e;
+        d = d - e;
+
+        System.out.println(d + " " + e);
     }
 }
