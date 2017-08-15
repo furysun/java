182ef69d80d4de2da6e611c37d29eaa6b4842cc8
furysun <fury.sun.main@gmail.com>
2017-08-15 15:15:01 +0300

    Worked with class stringTwo

--- src/com/company/codingCat/StringTwo.java
+++ src/com/company/codingCat/StringTwo.java
@@ -8,8 +8,57 @@ public class StringTwo {
 
     }
 
+//    public String middleTwo(String str) {
+//
+//    }
+
+    public String withouEnd2(String str) {
+        if (str.length() < 3) {
+            return "";
+        } else {
+            return str.substring(1, str.length() - 1);
+        }
+    }
+
+    public String theEnd(String str, boolean front) {
+
+        if (front) {
+            return str.substring(0, 1);
+        } else {
+            return str.substring(str.length() - 1);
+        }
+    }
+
+    public String right2(String str) {
+        if (str.length() < 1) {
+            return str;
+        } else {
+            return str.substring(str.length() - 2) + str.substring(0, str.length() - 2);
+        }
+    }
+
+    public String left2(String str) {
+        if (str.length() < 1) {
+            return str;
+        } else {
+            return str.substring(2) + str.substring(0, 2);
+        }
+    }
 
 
+    public String nonStart(String a, String b) {
+        return a.substring(1) + b.substring(1);
+    }
+
+    public String comboString(String a, String b) {
+        if (a.length() > b.length()) {
+            return b + a + b;
+        } else {
+            return a + b + a;
+        }
+    }
+
+    //
     public static String m(String str) {
 
         if (str.length() > 3 || !str.contains("o")) {
@@ -36,7 +85,7 @@ public class StringTwo {
         if (str.length() > 2) {
             return str.substring(0, 2);
         }
-            return str;
+        return str;
     }
 
 
