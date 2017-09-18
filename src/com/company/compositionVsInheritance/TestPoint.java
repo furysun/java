15de3419a6dc3bbb3bce0dd27e45a657e0ee6ab5
furysun <fury.sun.main@gmail.com>
2017-09-18 14:05:59 +0300

    added 2 packages and many classes

---
+++ src/com/company/compositionVsInheritance/TestPoint.java
@@ -0,0 +1,9 @@
+package com.company.compositionVsInheritance;
+
+public class TestPoint {
+    public static void main(String[] args) {
+        Point p1 = new Point(10, 20);
+        System.out.println(p1);
+        p1.setYX(100,10);
+    }
+}
