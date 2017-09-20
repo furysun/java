6edf31a6e8673f8708884344daccdd282e71ffff
furysun <fury.sun.main@gmail.com>
2017-09-20 14:51:54 +0300

    added 1 packages and 5 classes

---
+++ src/com/company/polimorphism/Animal.java
@@ -0,0 +1,5 @@
+package com.company.polimorphism;
+
+abstract public class Animal {
+    abstract public void greeting();
+}
---
+++ src/com/company/polimorphism/Cat.java
@@ -0,0 +1,8 @@
+package com.company.polimorphism;
+
+public class Cat extends Animal {
+    @Override
+    public void greeting() {
+        System.out.println("Meow");
+    }
+}
