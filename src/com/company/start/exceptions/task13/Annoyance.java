5ecd9e0ff7a775eac6559963f8e8fcec32ec5f98
furysun <fury.sun.main@gmail.com>
2017-10-02 12:48:26 +0300

    one package and 3 classes are added.

---
+++ src/com/company/start/exceptions/task13/Annoyance.java
@@ -0,0 +1,25 @@
+package com.company.start.exceptions.task13;
+
+public class Annoyance extends Exception {
+
+}
+
+class Sneeze extends Annoyance {
+}
+
+class Human {
+    public static void main(String[] args) {
+        try {
+            throw new Sneeze();
+        } catch (Sneeze s) {
+            System.out.println("catch Sneeze");
+        } catch (Annoyance a) {
+            System.out.println("catch Annoyance");
+        }
+        try {
+            throw new Sneeze();
+        }catch (Annoyance a){
+            System.out.println("catch Annoyance");
+        }
+    }
+}
