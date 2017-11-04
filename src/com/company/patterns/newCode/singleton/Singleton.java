5b133457a250141e9bceb002c7410785a7becd64
furysun <fury.sun.main@gmail.com>
2017-11-04 14:25:03 +0200

    new folder and class

---
+++ src/com/company/patterns/newCode/singleton/Singleton.java
@@ -0,0 +1,22 @@
+package com.company.patterns.newCode.singleton;
+
+public final class Singleton {
+    private static Singleton instance;
+    public String value;
+
+    private Singleton(String value) {
+        try {
+            Thread.currentThread().sleep(1000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        this.value = value;
+    }
+
+    public static Singleton getInstance(String value) {
+        if (instance == null) {
+            instance = new Singleton(value);
+        }
+        return instance;
+    }
+}
